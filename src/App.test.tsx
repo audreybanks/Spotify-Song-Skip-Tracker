9ee6dacda8b81a349bcd719443372b4ccf2b3e00
audreybanks <68301420+audreybanks@users.noreply.github.com>
2024-01-25 14:26:08 -0500

    Added Authentication

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+const signOut = () => {
+  console.log("sign out test");
+}
+
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App signOut={signOut}/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
